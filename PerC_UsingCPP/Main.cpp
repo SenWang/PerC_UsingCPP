281000813d28d548c3c64627b9f1271cb654a013
Sen Wang <moli.wang@gmail.com>
2013-06-25 15:25:42 +0800

    init

---
+++ PerC_UsingCPP/Main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+
+#include "util_render.h"
+#include "util_pipeline.h"
+ 
+
+int main(int argc, char* argv[]) 
+{
+	using namespace std ;
+	cout << "啟動攝影機" << endl ;
+	UtilPipeline pp;
+	pp.EnableImage(PXCImage::COLOR_FORMAT_RGB32);
+	pp.EnableImage(PXCImage::COLOR_FORMAT_DEPTH);
+	pp.Init();
+
+	cout << "設定GUI" << endl ;
+	UtilRender color_render(L"彩色影像");
+	UtilRender depth_render(L"深度影像");
+
+	cout << "開始接收影像" << endl ;
+	for (;;) 
+	{
+		if (!pp.AcquireFrame(true)) 
+			break;
+
+		PXCImage *color_image=pp.QueryImage(PXCImage::IMAGE_TYPE_COLOR);
+		PXCImage *depth_image=pp.QueryImage(PXCImage::IMAGE_TYPE_DEPTH);
+
+		if (!color_render.RenderFrame(color_image)) 
+			break;
+		if (!depth_render.RenderFrame(depth_image)) 
+			break;
+		pp.ReleaseFrame();
+	}
+	pp.Close();
+	cout << "關閉攝影機" << endl ;
+
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+﻿PerC SDK範例程式碼 : 使用 C++
+參考文件 http://software.intel.com/sites/landingpage/perceptual_computing/documentation/html/index.html?docm_my_first_c_application.html
